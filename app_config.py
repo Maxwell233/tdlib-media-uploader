4f57d7b9f3055a4f9b6b62333f9bb1247dec7b6a
Maximum <33753009+Maxwell233@users.noreply.github.com>
2026-09-03 03:11:36 +0200

    Update app version to 1.6.4

--- app_config.py
+++ app_config.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 import tomllib
 from pathlib import Path
 
-APP_VERSION = "1.6.3"
+APP_VERSION = "1.6.4"
 
 PROJECT_DIR = Path(__file__).resolve().parent
 CONFIG_PATH = PROJECT_DIR / "config.toml"
